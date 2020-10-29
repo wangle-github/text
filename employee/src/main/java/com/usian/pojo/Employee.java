6535fda420708e555c354dbc48c85097fa942c2f
wangle-github <1761431010@qq.com>
2020-10-29 11:38:51 +0800

    修改实体类

--- employee/src/main/java/com/usian/pojo/Employee.java
+++ employee/src/main/java/com/usian/pojo/Employee.java
@@ -17,6 +17,22 @@ public class Employee {
     private String starttime;
     private String endtime;
 
+    public String getStarttime() {
+        return starttime;
+    }
+
+    public void setStarttime(String starttime) {
+        this.starttime = starttime;
+    }
+
+    public String getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(String endtime) {
+        this.endtime = endtime;
+    }
+
     public Employee() {
     }
 
